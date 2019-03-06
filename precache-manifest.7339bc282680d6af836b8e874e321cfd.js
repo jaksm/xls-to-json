5305009dd245903955724fa7548f862bea82695e
Jaksa Malisic <jaksa.malisic@gmail.com>
2019-03-06 15:22:25 +0100

    Updates

---
+++ precache-manifest.7339bc282680d6af836b8e874e321cfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cffbbb88e4b51c59e81",
+    "url": "/xls-to-json/static/css/main.b0f2e81c.chunk.css"
+  },
+  {
+    "revision": "3cffbbb88e4b51c59e81",
+    "url": "/xls-to-json/static/js/main.3cffbbb8.chunk.js"
+  },
+  {
+    "revision": "5122ef5bf9094cbe18bd",
+    "url": "/xls-to-json/static/js/runtime~main.5122ef5b.js"
+  },
+  {
+    "revision": "d7e6e64ae397c9505a59",
+    "url": "/xls-to-json/static/js/2.d7e6e64a.chunk.js"
+  },
+  {
+    "revision": "30d856a6fed7c973a59e4f8d42c6da31",
+    "url": "/xls-to-json/index.html"
+  }
+];
