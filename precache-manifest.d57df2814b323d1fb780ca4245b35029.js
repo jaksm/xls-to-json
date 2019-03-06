de59ed33fc2ed3bf2adad3c785adf2adbcf56101
Jaksa Malisic <jaksa.malisic@gmail.com>
2019-03-06 14:45:54 +0100

    first commit

---
+++ precache-manifest.d57df2814b323d1fb780ca4245b35029.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33b0d8d34cd08c589f26",
+    "url": "/static/css/main.b0f2e81c.chunk.css"
+  },
+  {
+    "revision": "33b0d8d34cd08c589f26",
+    "url": "/static/js/main.33b0d8d3.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d7e6e64ae397c9505a59",
+    "url": "/static/js/2.d7e6e64a.chunk.js"
+  },
+  {
+    "revision": "8660051dd9a39998c835cf2aad7bd5a3",
+    "url": "/index.html"
+  }
+];
